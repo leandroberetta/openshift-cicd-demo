50ec8f114105f41182b66e361a691135c421c0d7
Leandro Beretta <lea.beretta@gmail.com>
2017-09-01 12:53:55 -0300

    changes

--- src/main/java/com/redhat/gps/service/BundleService.java
+++ src/main/java/com/redhat/gps/service/BundleService.java
@@ -30,9 +30,9 @@ public class BundleService {
 
         Properties properties = this.loadProperties("bundle.properties");
 
-        bundleInfo.setVersion(properties.getProperty("version"));
-        bundleInfo.setArtifactId(properties.getProperty("artifactId"));
-        bundleInfo.setGroupId(properties.getProperty("groupId"));
+        bundleInfo.setVersion(properties.getProperty("bundle.version"));
+        bundleInfo.setArtifactId(properties.getProperty("bundle.artifactId"));
+        bundleInfo.setGroupId(properties.getProperty("bundle.groupId"));
 
         return bundleInfo;
     }
@@ -41,31 +41,12 @@ public class BundleService {
         InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
         Properties properties = new Properties();
 
-
-        String text = null;
-        try (Scanner scanner = new Scanner(is, StandardCharsets.UTF_8.name())) {
-            text = scanner.useDelimiter("\\A").next();
-
-            logger.info(text);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
         if (is != null) {
-            System.out.println("bundle.properties present");
-            logger.info("bundle.properties present");
-
             try {
                 properties.load(is);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            logger.info(properties.getProperty("version"));
-
-        } else {
-            System.out.println("bundle.properties not present");
-            logger.error("bundle.properties not present");
         }
 
         return properties;
