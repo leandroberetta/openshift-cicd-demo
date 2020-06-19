c2c2d6e0e19bbd619eaceeebb7f8448ccdff1888
Leandro Beretta <lberetta@redhat.com>
2020-06-18 21:45:40 -0300

    fix demo sh

--- demo/scripts/setup.sh
+++ demo/scripts/setup.sh
@@ -9,4 +9,4 @@ oc new-app --template=jenkins-ephemeral --name=jenkins -n hello-dev
 oc adm policy add-role-to-user edit system:serviceaccount:hello-dev:jenkins -n hello-test
 oc adm policy add-role-to-user edit system:serviceaccount:hello-dev:jenkins -n hello-prod
 
-oc new-build https://github.com/leandroberetta/openshift-cicd-demo --name=hello-pipeline --strategy=pipeline -e APP_NAME=hello -n dev
+oc new-build https://github.com/leandroberetta/openshift-cicd-demo --name=hello-pipeline --strategy=pipeline -e APP_NAME=hello -n hello-dev
